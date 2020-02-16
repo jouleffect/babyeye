b4e25abadc9f3108c39fe531466a6bb32b8b1cc8
Giulia Maraventano <53179989+jouleffect@users.noreply.github.com>
2020-02-16 23:19:37 +0100

    Update clean.py

--- trainer/clean.py
+++ trainer/clean.py
@@ -30,3 +30,27 @@ for i in range(30):
     plt.axis('off')
 plt.subplots_adjust(wspace=-0.1, hspace=-0.1)
 plt.show()
+
+
+#-------Aumento dei dati---------------#
+datagen = ImageDataGenerator(
+        rotation_range=10,  
+        zoom_range = 0.10,  
+        width_shift_range=0.1, 
+        height_shift_range=0.1)
+
+#-------Immagini di esempio---------------#
+X_train3 = X_train[9,].reshape((1,28,28,1))
+Y_train3 = y_train[9,].reshape((1,10))
+plt.figure(figsize=(15,4.5))
+for i in range(30):  
+    plt.subplot(3, 10, i+1)
+    X_train2, Y_train2 = datagen.flow(X_train3,Y_train3).next()
+    plt.imshow(X_train2[0].reshape((28,28)),cmap=plt.cm.binary)
+    plt.axis('off')
+    if i==9: X_train3 = X_train[11,].reshape((1,28,28,1))
+    if i==19: X_train3 = X_train[18,].reshape((1,28,28,1))
+plt.subplots_adjust(wspace=-0.1, hspace=-0.1)
+plt.show()
+
+
