72f1bbae03c6b9c593a68c2d524a74898ced5987
Giulia Maraventano <53179989+jouleffect@users.noreply.github.com>
2020-02-16 23:28:16 +0100

    Update predict.py

--- trainer/predict.py
+++ trainer/predict.py
@@ -1,1 +1,10 @@
+results = np.zeros( (test.shape[0],10) ) 
 
+for j in range(nets):
+    results = results + model[j].predict(test)
+    
+results = np.argmax(results,axis = 1)
+results = pd.Series(results,name="Label")
+
+sub = pd.concat([pd.Series(range(1,28001),name = "ImageId"),ris],axis = 1)
+sub.to_csv("prediction.csv",index=False)
