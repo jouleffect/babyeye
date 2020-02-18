c0be76f3630f099b435b99e646c5667122db3cce
Giulia Maraventano <53179989+jouleffect@users.noreply.github.com>
2020-02-18 09:52:29 +0100

    Update clean.py

--- trainer/clean.py
+++ trainer/clean.py
@@ -42,18 +42,5 @@ datagen = ImageDataGenerator(
         width_shift_range=0.1, 
         height_shift_range=0.1)
 
-#-------Immagini di esempio---------------#
-X_train3 = X_train[9,].reshape((1,28,28,1))
-Y_train3 = y_train[9,].reshape((1,10))
-plt.figure(figsize=(15,4.5))
-for i in range(30):  
-    plt.subplot(3, 10, i+1)
-    X_train2, Y_train2 = datagen.flow(X_train3,Y_train3).next()
-    plt.imshow(X_train2[0].reshape((28,28)),cmap=plt.cm.binary)
-    plt.axis('off')
-    if i==9: X_train3 = X_train[11,].reshape((1,28,28,1))
-    if i==19: X_train3 = X_train[18,].reshape((1,28,28,1))
-plt.subplots_adjust(wspace=-0.1, hspace=-0.1)
-plt.show()
 
 
