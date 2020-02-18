c3fb37f23afded65e0bb13da742744662ce5673b
Giulia Maraventano <53179989+jouleffect@users.noreply.github.com>
2020-02-18 11:43:31 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
+import os, os.path
+import sys
+from tensorflow.keras.models import load_model
+import warnings 
+warnings.filterwarnings('ignore')
 
+PATH = 'model/'
+
+nets = len([name for name in os.listdir(PATH)])
+
+model = [0] *nets
+
+for i in range(nets):
+	nome_file = PATH + str(i+1)+".h5"
+	model[i] = load_model(nome_file)
+
+print("Model succesfully loaded.")
+
+t = input("Do you want to test the data? (Make sure you put the test.csv file into the data path)")
