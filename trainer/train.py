9dae66e4d2f15e3ef96d65376cd231315f2bbbae
Giulia Maraventano <53179989+jouleffect@users.noreply.github.com>
2020-02-18 10:01:14 +0100

    Update train.py

--- trainer/train.py
+++ trainer/train.py
@@ -1,19 +1,22 @@
-
-from keras.callbacks import ReduceLROnPlateau
 from keras.callbacks import LearningRateScheduler
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
+import warnings 
+warnings.filterwarnings('ignore')
 
-learning_rate = LearningRateScheduler(lambda x: 1e-3 * 0.95 ** x)
+def train_1(X_train,y_train,model,nets,datagen):
 
-batch = 64
-epochs = 5
-H = [0] * nets
+	batch = input("-------| BATCH SIZE -------> ")
+	batch = int(batch)
+	epochs = input("-------| EPOCHS -------> ")
+	epochs = int(epochs)
 
-#datagen.fit(X_train)
+	learning_rate = LearningRateScheduler(lambda x: 1e-3 * 0.95 ** x)
+	
+	H = [0] * nets
 
-for j in range(nets):
-    X_train2, X_val2, y_train2, y_val2 = train_test_split(X_train, y_train, test_size = 0.1)
-    H[j] = model[j].fit_generator(datagen.flow(X_train2,y_train2, batch_size=batch),
-                              epochs = epochs, validation_data = (X_val2,y_val2),
-                              verbose = 1, steps_per_epoch=X_train2.shape[0] // batch
-                              ,callbacks=[learning_rate])
+	for j in range(nets):
+	    X_train2, X_val2, y_train2, y_val2 = train_test_split(X_train, y_train, test_size = 0.1)
+	    H[j] = model[j].fit_generator(datagen.flow(X_train2,y_train2, batch_size=batch),
+	                              epochs = epochs, validation_data = (X_val2,y_val2),
+	                              verbose = 1, steps_per_epoch=X_train2.shape[0] // batch
+	                              ,callbacks=[learning_rate])
